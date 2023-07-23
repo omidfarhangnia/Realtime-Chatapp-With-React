864b3d532e68207dff64ab9690fa36d820b347e9
omidfarhangnia <83111084+omidfarhangnia@users.noreply.github.com>
2023-07-23 15:06:34 +0330

    completeting profile structure

--- src/Pages/ShowProfile.jsx
+++ src/Pages/ShowProfile.jsx
@@ -1,28 +1,42 @@
-import { doc, updateDoc } from "firebase/firestore";
-import { useEffect, useRef, useState } from "react";
+import { doc, onSnapshot, refEqual, updateDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { db } from "../firebase";
 
 function ShowProfile({ currentUser }) {
   const location = useLocation();
-  const target = location.state.target;
-  const [description, setDescription] = useState(
-    target.description !== "" ? target.description : ""
-  );
-  const messageSenderName = target.name
-    .split(" ")
-    .map((text) => text[0])
-    .join("");
-  const inputRef = useRef(null);
+  const [target, setTarget] = useState({});
+  const [description, setDescription] = useState();
+  const [edit, setEdit] = useState(false);
+
+  useEffect(() => {
+    async function makeSnapShot() {
+      const unsub = onSnapshot(
+        doc(db, "messages", location.state.target.name),
+        (doc) => {
+          setTarget(doc.data());
+        }
+      );
+    }
+
+    return () => {
+      makeSnapShot();
+    };
+  }, [location, currentUser]);
+
+  useEffect(() => {
+    if (Object.keys(target).length === 0) return;
+
+    setDescription(target.description);
+  }, [target]);
 
   async function handleSaveDescription() {
-    await updateDoc(doc(db, "messages", target.name), {
-      description: inputRef.current.value,
+    await updateDoc(doc(db, "messages", location.state.target.name), {
+      description: description,
     })
       .then(() => {
-        target.description = inputRef.current.value;
-        setDescription(inputRef.current.value);
-        inputRef.current.value = "";
+        target.description = description;
+        setDescription(target.description);
       })
       .catch((e) => {
         console.log(e);
@@ -41,15 +55,22 @@ function ShowProfile({ currentUser }) {
                 bg-customBrown/60 flex justify-center items-center text-[80px] uppercase
               rounded-full`}
             >
-              <span>{messageSenderName}</span>
+              <span>
+                {target.name !== undefined &&
+                  target.name
+                    .split(" ")
+                    .map((text) => text[0])
+                    .join("")}
+              </span>
             </div>
             <div className="text-[30px] font-poppins">{target.name}</div>
           </div>
           {/* <div>is he online</div> */}
-          {description === "" && target.name === currentUser.name ? (
+          {target.description === "" && target.name === currentUser.name ? (
             <div className="flex justify-between items-center">
               <input
-                ref={inputRef}
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
                 className="bg-transparent text-[20px] py-2 px-4 placeholder:capitalize rounded-full border-2 border-solid border-customLightBlue bg-white text-black placeholder:text-gray-500 focus-within:outline-none font-poppins "
                 placeholder="enter your description"
               />
@@ -61,7 +82,45 @@ function ShowProfile({ currentUser }) {
               </button>
             </div>
           ) : (
-            <div className="text-center w-full p-3 bg-black/25 rounded-lg text-[18px]">{target.description === "" ? "there is no description" : target.description}</div>
+            <div className="text-center w-full p-3 bg-black/25 rounded-lg text-[25px] font-bold">
+              {target.description === "" ? "" : description}
+            </div>
+          )}
+
+          {target.description !== "" && target.name === currentUser.name && (
+            <div className="flex justify-between items-center">
+              {edit ? (
+                <textarea
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                  className="w-[50%] min-h-[100px] bg-transparent border-2 border-solid border-customLightBlue/30 rounded-lg resize-none px-3 py-2 focus-within:outline-none text-[18px] font-poppins messageScrollBar"
+                />
+              ) : (
+                <span className="block w-[50%] min-h-[100px]"></span>
+              )}
+              {edit ? (
+                <button
+                  style={{ background: target.userColor }}
+                  className="text-[20px] font-poppins capitalize py-2 px-4 rounded-lg"
+                  onClick={() => {
+                    setEdit(!edit);
+                    handleSaveDescription();
+                  }}
+                >
+                  save description
+                </button>
+              ) : (
+                <button
+                  style={{ background: target.userColor }}
+                  className="text-[20px] font-poppins capitalize py-2 px-4 rounded-lg"
+                  onClick={() => {
+                    setEdit(!edit);
+                  }}
+                >
+                  edit description
+                </button>
+              )}
+            </div>
           )}
         </div>
       </div>
